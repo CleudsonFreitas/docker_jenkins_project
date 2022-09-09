df7fec5ab3e1849b9d3c3680bda7de2a20368438
Cleudson Fernandes de Freitas <cleudsonfreitas@gmail.com>
2022-09-09 15:08:52 -0300

    add project net 6

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jenkins-Docker net6");
+	    Console.ReadKey();
+        }
+    }
+}
